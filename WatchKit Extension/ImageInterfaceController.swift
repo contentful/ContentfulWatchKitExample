1fd6cce76a1b4716c9c528c0d2680e9f74c5c4bc
Boris Bügling <boris@icculus.org>
2014-12-05 17:26:39 +0100

    Animated loading spinner

--- WatchKit Extension/ImageInterfaceController.swift
+++ WatchKit Extension/ImageInterfaceController.swift
@@ -12,6 +12,24 @@ class ImageInterfaceController:BaseInterfaceController {
     @IBOutlet weak var firstImage: WKInterfaceImage!
     @IBOutlet weak var secondImage: WKInterfaceImage!
 
+    func generateLoadingAnimation() {
+        let sheet = UIImage(named: "activity-medium")
+        var images = [UIImage]()
+
+        for (var currentX = 0; currentX < Int(sheet!.size.width); currentX += 30) {
+            let splitRef = CGImageCreateWithImageInRect(sheet?.CGImage, CGRect(x: CGFloat(currentX), y: 0.0, width: 30.0, height: sheet!.size.height * 2))
+            images.append(UIImage(CGImage: splitRef)!)
+        }
+
+        let image = UIImage.animatedImageWithImages(images, duration: 0.1)
+
+        firstImage.setImage(image)
+        firstImage.startAnimating()
+
+        secondImage.setImage(image)
+        secondImage.startAnimating()
+    }
+
     override init(context: AnyObject?) {
         super.init(context: context)
 
@@ -25,14 +43,20 @@ class ImageInterfaceController:BaseInterfaceController {
             return
         }
 
+        generateLoadingAnimation()
+
         fetchAsset(images[0], completionHandler: { (image) in
             if images.count == 1 {
+                self.firstImage.stopAnimating()
                 self.firstImage.setImage(image)
                 return
             }
 
             self.fetchAsset(images[1], completionHandler: { (image2) in
+                self.firstImage.stopAnimating()
                 self.firstImage.setImage(image)
+
+                self.secondImage.stopAnimating()
                 self.secondImage.setImage(image2)
             })
         })
